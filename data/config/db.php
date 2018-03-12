679dccca23befe5aa2b54453de047fa74b54afae
tangbq <tangbq@tiens.com>
2018-03-12 10:18:13 +0800

    update from dell msysgit

--- data/config/db.php
+++ data/config/db.php
@@ -1,1 +1,10 @@
-<?php return [];?>
+<?php return array (
+  'class' => 'yii\\db\\Connection',
+  'dsn' => 'mysql:host=10.0.75.1;dbname=lulucms2',
+  'username' => 'root',
+  'password' => 'root',
+  'charset' => 'utf8',
+  'tablePrefix' => 'lulu_',
+  'enableSchemaCache' => true,
+  'schemaCache' => 'schemaCache',
+) ;?>
